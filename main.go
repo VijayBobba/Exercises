5d48f5428ed4d00e2f1a5c41dbcec3b06e646403
Vijay <forbvijay@gmail.com>
2023-01-10 07:28:10 -0500

    comments added

--- main.go
+++ main.go
@@ -27,32 +27,32 @@ func numIslands() {
 	//}
 
 	//Case 3
-	//grid := [][]byte{
-	//	{1, 1, 1, 0, 1},
-	//	{0, 1, 0, 1, 0},
-	//	{1, 1, 0, 0, 1},
-	//	{1, 0, 0, 1, 0},
-	//}
+	grid := [][]byte{
+		{1, 1, 1, 0, 1},
+		{0, 1, 0, 1, 0},
+		{1, 1, 0, 0, 1},
+		{1, 0, 0, 1, 0},
+	}
 
 	//Case 4
-	grid := [][]byte{
-		{1, 0, 1, 0, 1, 0},
-		{0, 1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1, 0},
-		{0, 1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1, 0},
-	}
+	//grid := [][]byte{
+	//	{1, 0, 1, 0, 1, 0},
+	//	{0, 1, 0, 1, 0, 1},
+	//	{1, 0, 1, 0, 1, 0},
+	//	{0, 1, 0, 1, 0, 1},
+	//	{1, 0, 1, 0, 1, 0},
+	//}
 
 	//In the Go language, you are allowed to assign a function to a variable. When
 	//you assign a function to a variable, then the type of the variable is of
 	//function type, and you can call that variable like a function call
 	var dfs func(r, c int) bool
 	dfs = func(r, c int) bool {
-		if grid[r][c] != 1 {
-			return false
+		if grid[r][c] != 1 { // graph search starts with 1 in pos
+			return false // return to root or recursive calls in the process of graph search
 		}
 		grid[r][c] = 2
-		if r-1 >= 0 {
+		if r-1 >= 0 { //see that you are not in water (out of bounds)
 			dfs(r-1, c)
 		}
 		if r+1 < len(grid) {
@@ -64,12 +64,12 @@ func numIslands() {
 		if c+1 < len(grid[r]) {
 			dfs(r, c+1)
 		}
-		return true
+		return true // return to root
 	}
 	n := 0
 	for r := range grid {
 		for c := range grid[r] {
-			if dfs(r, c) {
+			if dfs(r, c) { //root
 				n++
 			}
 		}
