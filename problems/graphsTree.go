0f03ad614a35d206e69ad7d93da401f39676c04a
Vijay <forbvijay@gmail.com>
2023-01-17 09:03:43 -0500

    more funcs

---
+++ problems/graphsTree.go
@@ -0,0 +1,14 @@
+package problems
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func InvertTree(root *TreeNode) *TreeNode {
+	if root != nil {
+		root.Left, root.Right = InvertTree(root.Right), InvertTree(root.Left)
+	}
+	return root
+}
